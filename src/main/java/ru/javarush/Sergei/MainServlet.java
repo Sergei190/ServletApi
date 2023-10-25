9466d76272b22ba431788ca8c7cd124df6f59454
Sergei190 <kudrasovcergej@gmail.com>
2023-10-25 20:29:52 +0500

    last commit

---
+++ src/main/java/ru/javarush/Sergei/MainServlet.java
@@ -0,0 +1,36 @@
+package ru.javarush.Sergei;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class MainServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        Integer visitCounter = (Integer) session.getAttribute("visitCounter");
+        if (visitCounter == null) {
+            visitCounter = 1;
+        } else {
+            visitCounter++;
+        }
+        session.setAttribute("visitCounter", visitCounter);
+        String username = req.getParameter("username");
+        resp.setContentType("txt/html");
+        PrintWriter printWriter = resp.getWriter();
+        if (username == null) {
+            printWriter.write("Hello, Anonymous" + "<dir>");
+        } else {
+            printWriter.write("Hello, " + username + "<br>");
+        }
+        printWriter.write("Page was visited " + visitCounter + " times.");
+        printWriter.close();
+    }
+}
